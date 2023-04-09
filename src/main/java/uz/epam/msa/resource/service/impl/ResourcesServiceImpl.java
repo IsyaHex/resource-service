13e7686cb6830b6ceb09419639c559b86a41e359
IsyaHex <i.domlajanov@gmail.com>
2023-04-09 14:47:55 +0500

    Resilience functionality -> CircuitBreaker implementation, tests updates, additional api call for receiving storage types creation. Docker configurations updates. Additional dependencies inclusion

--- src/main/java/uz/epam/msa/resource/service/impl/ResourcesServiceImpl.java
+++ src/main/java/uz/epam/msa/resource/service/impl/ResourcesServiceImpl.java
@@ -2,9 +2,11 @@ package uz.epam.msa.resource.service.impl;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.vavr.control.Try;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,21 +14,22 @@ import uz.epam.msa.resource.constant.Constants;
 import uz.epam.msa.resource.domain.Resource;
 import uz.epam.msa.resource.dto.AudioDataBinaryDTO;
 import uz.epam.msa.resource.dto.DeletedResourcesDTO;
+import uz.epam.msa.resource.dto.GetStorageDTO;
 import uz.epam.msa.resource.dto.ResourceDTO;
-import uz.epam.msa.resource.exception.InternalServerErrorException;
 import uz.epam.msa.resource.exception.ResourceNotFoundException;
 import uz.epam.msa.resource.exception.ResourceValidationException;
 import uz.epam.msa.resource.repository.ResourcesRepository;
 import uz.epam.msa.resource.service.ResourcesService;
 import uz.epam.msa.resource.util.AwsUtil;
+import uz.epam.msa.resource.util.MicroserviceUtil;
+import uz.epam.msa.resource.util.ResourceUtil;
 
 import javax.transaction.Transactional;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 @Service
@@ -37,16 +40,18 @@ public class ResourcesServiceImpl implements ResourcesService {
     private final ModelMapper mapper;
     private final AmazonS3 s3Client;
     private final AwsUtil awsUtil;
+    private final ResourceUtil resourceUtil;
+    private final MicroserviceUtil microserviceUtil;
+    @Autowired
+    private CircuitBreaker circuitBreaker;
 
-    @Value("${aws.cloud.bucket.name}")
-    private String bucketName;
-
-
-    public ResourcesServiceImpl(ResourcesRepository repository, ModelMapper mapper, AmazonS3 s3Client, AwsUtil awsUtil) {
+    public ResourcesServiceImpl(ResourcesRepository repository, ModelMapper mapper, AmazonS3 s3Client, AwsUtil awsUtil, ResourceUtil resourceUtil, MicroserviceUtil microserviceUtil) {
         this.repository = repository;
         this.mapper = mapper;
         this.s3Client = s3Client;
         this.awsUtil = awsUtil;
+        this.resourceUtil = resourceUtil;
+        this.microserviceUtil = microserviceUtil;
     }
 
     @Override
@@ -54,8 +59,24 @@ public class ResourcesServiceImpl implements ResourcesService {
         Resource resource = repository.findById(id)
                 .filter(res -> !res.isDeleted())
                 .orElseThrow(() -> new ResourceNotFoundException(Constants.RESOURCE_NOT_FOUND_EXCEPTION));
+
+        GetStorageDTO permanentStorage = resourceUtil.getCircuitBreakerObject(
+                microserviceUtil::getPermanentStorage, microserviceUtil.getPermanentStorageFallBack());
+        log.info(String.format("Permanent storage id -> %s", permanentStorage.getId()));
         return new ResourceDTO(resource.getContentType(),
-                awsUtil.downloadFile(resource.getId() + Constants.UNDERSCORE + resource.getName()));
+                awsUtil.downloadFile(resource.getId() + Constants.UNDERSCORE + resource.getName(), permanentStorage.getBucket()));
+    }
+
+    @Override
+    public ResourceDTO findByIdInternal(Integer id) {
+        Resource resource = repository.findById(id)
+                .filter(res -> !res.isDeleted())
+                .orElseThrow(() -> new ResourceNotFoundException(Constants.RESOURCE_NOT_FOUND_EXCEPTION));
+        GetStorageDTO stagingStorage = resourceUtil.getCircuitBreakerObject(
+                microserviceUtil::getStagingStorage, microserviceUtil.getStagingStorageFallBack());
+        log.info(String.format("Staging storage id -> %s", stagingStorage.getId()));
+        return new ResourceDTO(resource.getContentType(),
+                awsUtil.downloadFile(resource.getId() + Constants.UNDERSCORE + resource.getName(), stagingStorage.getBucket()));
     }
 
     @Override
@@ -70,10 +91,13 @@ public class ResourcesServiceImpl implements ResourcesService {
             resource.setSize(data.getSize());
             resource.setDeleted(false);
             resource = repository.save(resource);
-            File file = convertMultipartFileToFile(data);
-            s3Client.putObject(new PutObjectRequest(bucketName,
+            File file = resourceUtil.convertMultipartFileToFile(data);
+            GetStorageDTO stagingStorage = resourceUtil.getCircuitBreakerObject(
+                    microserviceUtil::getStagingStorage, microserviceUtil.getStagingStorageFallBack());
+            s3Client.putObject(new PutObjectRequest(stagingStorage.getBucket(),
                     resource.getId() + Constants.UNDERSCORE + resource.getName(), file));
             file.delete();
+            log.info(String.format("Staging storage id -> %s", stagingStorage.getId()));
         } catch (Exception e) {
             log.error(e.getMessage());
             throw new ResourceValidationException(Constants.VALIDATION_EXCEPTION);
@@ -84,6 +108,14 @@ public class ResourcesServiceImpl implements ResourcesService {
     @Override
     public DeletedResourcesDTO deleteResources(String ids) {
         DeletedResourcesDTO dto = new DeletedResourcesDTO();
+
+        GetStorageDTO stagingStorage = resourceUtil.getCircuitBreakerObject(
+                microserviceUtil::getStagingStorage, microserviceUtil.getStagingStorageFallBack());
+        GetStorageDTO permanentStorage = resourceUtil.getCircuitBreakerObject(
+                microserviceUtil::getPermanentStorage, microserviceUtil.getPermanentStorageFallBack());
+        log.info(String.format("Staging storage id -> %s", stagingStorage.getId()));
+        log.info(String.format("Permanent storage id -> %s", permanentStorage.getId()));
+
         dto.setIds(Arrays.stream(ids.split(Constants.COMMA_REGEX))
                 .filter(id -> id.matches(Constants.NUMBER_REGEX))
                 .map(id -> repository.findById(Integer.parseInt(id)))
@@ -91,21 +123,12 @@ public class ResourcesServiceImpl implements ResourcesService {
                 .map(Optional::get)
                 .filter(file -> !file.isDeleted())
                 .peek(file -> file.setDeleted(true))
-                .peek(file -> awsUtil.deleteFile(file.getId() + Constants.UNDERSCORE + file.getName()))
+                .peek(file -> awsUtil.deleteFile(file.getId() + Constants.UNDERSCORE + file.getName(), stagingStorage.getBucket()))
+                .peek(file -> awsUtil.deleteFile(file.getId() + Constants.UNDERSCORE + file.getName(), permanentStorage.getBucket()))
                 .map(repository::save)
                 .map(Resource::getId)
                 .collect(Collectors.toList()));
         return dto;
     }
 
-    private File convertMultipartFileToFile(MultipartFile file) {
-        File convertedFile = new File(Objects.requireNonNull(file.getOriginalFilename()));
-        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
-            fos.write(file.getBytes());
-        } catch (IOException e) {
-            log.error(e.getMessage());
-            throw new InternalServerErrorException();
-        }
-        return convertedFile;
-    }
 }
--- src/test/java/uz/epam/msa/resource/service/impl/ResourcesServiceImplTest.java
+++ src/test/java/uz/epam/msa/resource/service/impl/ResourcesServiceImplTest.java
@@ -12,12 +12,16 @@ import org.springframework.web.multipart.MultipartFile;
 import uz.epam.msa.resource.domain.Resource;
 import uz.epam.msa.resource.dto.AudioDataBinaryDTO;
 import uz.epam.msa.resource.dto.DeletedResourcesDTO;
+import uz.epam.msa.resource.dto.GetStorageDTO;
 import uz.epam.msa.resource.dto.ResourceDTO;
 import uz.epam.msa.resource.repository.ResourcesRepository;
 import uz.epam.msa.resource.service.ResourcesService;
 import uz.epam.msa.resource.util.AwsUtil;
+import uz.epam.msa.resource.util.MicroserviceUtil;
+import uz.epam.msa.resource.util.ResourceUtil;
 
 import javax.naming.SizeLimitExceededException;
+import java.io.File;
 import java.util.List;
 import java.util.Optional;
 
@@ -38,10 +42,14 @@ public class ResourcesServiceImplTest {
     private AmazonS3 s3Client;
     @Mock
     private AwsUtil awsUtil;
+    @Mock
+    private ResourceUtil resourceUtil;
+    @Mock
+    private MicroserviceUtil microserviceUtil;
 
     @BeforeEach
     void init () {
-        service = new ResourcesServiceImpl(repository, mapper, s3Client, awsUtil);
+        service = new ResourcesServiceImpl(repository, mapper, s3Client, awsUtil, resourceUtil, microserviceUtil);
     }
 
     @Test
@@ -49,15 +57,52 @@ public class ResourcesServiceImplTest {
         String contentType = "audio/mpeg";
         Resource resource = getResource();
         String fileName = resource.getId() + "_" + resource.getName();
+        String bucketName = "testBucket";
 
         ResourceDTO dto = new ResourceDTO();
         dto.setResource(new byte[]{});
         dto.setContentType(contentType);
 
+        GetStorageDTO storageDTO = new GetStorageDTO();
+        storageDTO.setId(-1);
+        storageDTO.setPath("");
+        storageDTO.setBucket("testBucket");
+        storageDTO.setStorageType("testStorageType");
+
+        resourceUtil.getCircuitBreakerObject(
+                microserviceUtil::getPermanentStorage, microserviceUtil.getPermanentStorageFallBack());
+
         when(repository.findById(0)).thenReturn(Optional.of(resource));
-        when(awsUtil.downloadFile(fileName)).thenReturn(new byte[]{});
+        when(awsUtil.downloadFile(fileName, bucketName)).thenReturn(new byte[]{});
+        when(resourceUtil.getCircuitBreakerObject(any(), any())).thenReturn(storageDTO);
 
         ResourceDTO result = service.findById(0);
+
+        assertEquals(dto, result);
+    }
+
+    @Test
+    void findByIdInternal() {
+        String contentType = "audio/mpeg";
+        Resource resource = getResource();
+        String fileName = resource.getId() + "_" + resource.getName();
+        String bucketName = "testBucket";
+
+        ResourceDTO dto = new ResourceDTO();
+        dto.setResource(new byte[]{});
+        dto.setContentType(contentType);
+
+        GetStorageDTO storageDTO = new GetStorageDTO();
+        storageDTO.setId(-1);
+        storageDTO.setPath("");
+        storageDTO.setBucket("testBucket");
+        storageDTO.setStorageType("testStorageType");
+
+        when(repository.findById(0)).thenReturn(Optional.of(resource));
+        when(awsUtil.downloadFile(fileName, bucketName)).thenReturn(new byte[]{});
+        when(resourceUtil.getCircuitBreakerObject(any(), any())).thenReturn(storageDTO);
+
+        ResourceDTO result = service.findByIdInternal(0);
 
         assertEquals(dto, result);
     }
@@ -76,8 +121,16 @@ public class ResourcesServiceImplTest {
         dto.setSize(size);
         MultipartFile file = new MockMultipartFile(name, name, contentType, new byte[99999]);
 
+        GetStorageDTO storageDTO = new GetStorageDTO();
+        storageDTO.setId(-1);
+        storageDTO.setPath("");
+        storageDTO.setBucket("testBucket");
+        storageDTO.setStorageType("testStorageType");
+
         when(repository.save(any(Resource.class))).thenReturn(resource);
         when(mapper.map(resource, AudioDataBinaryDTO.class)).thenReturn(dto);
+        when(resourceUtil.convertMultipartFileToFile(file)).thenReturn(new File(file.getName()));
+        when(resourceUtil.getCircuitBreakerObject(any(), any())).thenReturn(storageDTO);
 
         assertEquals(dto, service.saveResource(file));
     }
@@ -90,8 +143,16 @@ public class ResourcesServiceImplTest {
 
         Resource resource = getResource();
 
+        GetStorageDTO storageDTO = new GetStorageDTO();
+        storageDTO.setId(-1);
+        storageDTO.setPath("");
+        storageDTO.setBucket("testBucket");
+        storageDTO.setStorageType("testStorageType");
+
         when(repository.findById(any(Integer.class))).thenReturn(Optional.of(resource));
         when(repository.save(any(Resource.class))).thenReturn(resource);
+        when(resourceUtil.getCircuitBreakerObject(any(), any())).thenReturn(storageDTO);
+
         assertEquals(dto, service.deleteResources("99999"));
     }
 
